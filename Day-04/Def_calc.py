31a06d9d2589d6cd66173dad42b5bf74d3b6ecad
rajendra <rajendrabhadane2012@gmail.com>
2023-10-27 23:55:14 +0530

    added into day4

---
+++ Day-04/Def_calc.py
@@ -0,0 +1,19 @@
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+
+def addition():      ##function
+    add= num1 + num2
+    print(add)
+
+def subtraction():
+    sub= num2 - num1
+    print(sub)
+
+def multiplication():
+    mul= num1 * num2
+    print(mul)
+
+
+addition()
+subtraction()
+multiplication()
---
+++ Day-04/Simple_Calcultor.py
@@ -0,0 +1,11 @@
+num1=10
+num2=20
+
+addition = num1 + num2    ##addi
+print("addition of two numbers is:" + str(addition))
+
+subtraction = num2 - num1
+print("Subtraction of two numbers is:" + str(subtraction))
+
+
+
