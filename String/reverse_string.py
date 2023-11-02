9de4c3f8f2f6ccb83d13fd8648d925c3c2054f62
rajendra <rajendrabhadane2012@gmail.com>
2023-11-02 17:00:54 +0530

    created folder for string and added files into that

---
+++ String/reverse_string.py
@@ -0,0 +1,21 @@
+
+# by using index
+from turtle import st
+
+
+a = "UNIVERSAL"
+print(a)
+print(" reverse string :" , a[::-1])
+
+
+# another way by using reversed () & join () functions
+
+print( "## another way ##" )
+
+strg = "INDIA"
+print(strg)
+
+reversed_strg = ' '.join(reversed(strg))
+
+print (reversed_strg)
+
