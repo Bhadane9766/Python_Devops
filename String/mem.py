580138a1670b2038873693e4a17027ff6dee866a
rajendra <rajendrabhadane2012@gmail.com>
2023-11-03 13:03:08 +0530

    added new files

---
+++ String/mem.py
@@ -0,0 +1,5 @@
+x = "python program"
+print('o' in x)
+print('t' in x)
+print('s' in x)
+
---
+++ String/replace_char.py
@@ -0,0 +1,5 @@
+
+str = "Apple"
+print(str)
+
+print( "output for replace char A with B :-" , str.replace("A" , "B"))
