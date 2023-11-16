ca3e512c54b9ada5c4a5ba4c380eda9e3977251f
rajendra <rajendrabhadane2012@gmail.com>
2023-11-16 23:49:08 +0530

    dict folder created

--- dictionary/dict.py
+++ dictionary/dict.py
@@ -1,8 +1,10 @@
+'''
 # Dictionary 
 
 # get key from dict
 
-dict = {"brand":"tata", "model":"nexon", "year":"2020"}
+dict = {"brand":"tata", "model":"nexon", "year":2020}
+print(len(dict))
 for x in dict:
     print(x)    # it will print key
 
@@ -20,8 +22,41 @@ for i in dict.values():
 
 #### item() function in dictionary 
 
-dict = {"brand":"tata", "model":"nexon", "year":"2020"}
+dict = {"brand":"tata", "model":"nexon", "year":2020}
 
 for x,y in dict.items():
     print(x,y)
 
+#### modify in dictionary
+
+dict1 = dict
+dict1["year"] = 2022
+print(dict1)
+dict1["model"] = "SUV"
+print(dict1)
+
+#### find key in dictionary
+
+if "model" in dict:
+    print("model key is present in dict")
+
+else:
+    print("key is not found")
+
+
+#### fromkeys function in dictionary
+
+x=('first_ket', 'second_key', 'third_key')
+y=123
+
+dict1 = dict.fromkeys(x,y)   # create new dictionary from keys and values 
+print(dict1)
+
+'''
+#### setdefault function in dictionary
+ # this function use for checking item exist or not incase item does not exit it will be added into dictionary.
+
+dict12 = {"brand":"tata", "model":"nexon", "year":2020}
+xy= dict12.setdefault("color","white")
+print(xy)
+print(dict12)
