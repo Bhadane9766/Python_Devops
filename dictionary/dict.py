4878eae2a0fa61e533abe8ff043c84691f22cbf4
rajendra <rajendrabhadane2012@gmail.com>
2023-11-16 22:42:57 +0530

    dict folder created

---
+++ dictionary/dict.py
@@ -0,0 +1,27 @@
+# Dictionary 
+
+# get key from dict
+
+dict = {"brand":"tata", "model":"nexon", "year":"2020"}
+for x in dict:
+    print(x)    # it will print key
+
+print("####### print key vaule") 
+
+
+## values() function for print keyvalue
+
+
+for y in dict:
+    print([y])   # it will print key value 
+
+for i in dict.values():
+    print(i)
+
+#### item() function in dictionary 
+
+dict = {"brand":"tata", "model":"nexon", "year":"2020"}
+
+for x,y in dict.items():
+    print(x,y)
+
