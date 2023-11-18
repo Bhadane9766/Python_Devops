ebcc45d5d2ca7c9441bf2c053bb8b04cbeb517b2
rajendra <rajendrabhadane2012@gmail.com>
2023-11-18 10:47:22 +0530

    file created in dict

---
+++ dictionary/student_info.py
@@ -0,0 +1,28 @@
+# program for student info and print the output
+
+student_DB={}     # create empty dictionary
+
+# take input from user
+while True:
+
+    line=input("enter the id and name seperated by comma or press q to exit:")
+
+    if line=='q':
+        break
+    id,name = line.split(',')
+    student_DB.update({id:name})
+
+# print the info
+
+for x,y in student_DB.items():
+    print(x,y)
+
+# find key 
+
+key= input("enter id of the student:")
+
+if key in student_DB:
+    print("ID is=", id, "value is=", student_DB([key]))
+
+else:
+    print("Given id is not found")    
