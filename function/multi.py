4105d606592a0885c715b1b88dbf1d56e47b05e2
rajendra <rajendrabhadane2012@gmail.com>
2023-11-23 08:58:05 +0530

    created function folder

---
+++ function/multi.py
@@ -0,0 +1,23 @@
+
+x = int(input("Enter the number:"))
+
+def mul(x):
+    for i in range(1,11):
+        y= x*i
+        print(f"the multiplication of {x}*{i}:{y}")
+
+def add(x):
+    for i in range(1,11):
+        y= x+i
+        
+        print(f"the addition of {y}+{i}:{y}")
+
+
+
+mul(x)
+add(x)
+
+
+
+
+
